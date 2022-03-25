e53ffa492a23dd2b8f12dd2d5348263521422901
tqxli <tianqing.li@duke.edu>
2022-03-25 15:20:26 -0400

    standardize loss formulation

--- dannce/engine/models_pytorch/loss.py
+++ dannce/engine/models_pytorch/loss.py
@@ -7,41 +7,7 @@ import torch
 import torch.nn.functional as F
 from dannce.engine.models_pytorch.body_limb import SYMMETRY
 
-def separation_loss(keypoints_3d, delta):
-    """
-    Penalize a pair of 3D keypoints if they are closer than a preset threshold delta.
-    Inputs:
-        keypoints_3d: Tensor [B, N, 3]
-        delta: scalar
-    """
-    bs, num_kpts = keypoints_3d.shape[0], keypoints_3d.shape[1]
-
-    t1 = keypoints_3d.repeat(1, num_kpts, 1)
-    t2 = keypoints_3d.repeat(1, 1, num_kpts).reshape(t1.shape)
-
-    diff = (t1 - t2)**2
-
-    dist = torch.sum(diff, dim=2) # [B, N**2]
-
-    return torch.sum(torch.maximum(delta-dist, 0)) / (num_kpts * bs *2)
-
-def sillhouette_loss(mask, heatmaps):
-    """
-    Loss for the keypoints to fall inside the 2D mask
-    Inputs:
-        mask: [B, H, W] binary
-        heatmaps: [B, n_joints, H, W]
-    """
-    mask_dist = F.softmax(mask.flatten()).reshape(mask.shape)
-    mask_dist = mask_dist.unsqueeze(1)
-
-    sill = torch.sum(mask_dist * heatmaps, dim=[2, 3])
-    sill = torch.sum(-torch.log(sill + 1e-12))
-
-    return sill
-
 def mask_nan(kpts_gt, kpts_pred):
-    # import pdb; pdb.set_trace()
     nan_gt = torch.isnan(kpts_gt)
     not_nan = (~nan_gt).sum()
     kpts_gt[nan_gt] = 0 
@@ -50,30 +16,94 @@ def mask_nan(kpts_gt, kpts_pred):
 
 def compute_mask_nan_loss(loss_fcn, kpts_gt, kpts_pred):
     kpts_gt, kpts_pred, notnan = mask_nan(kpts_gt, kpts_pred)
+    # when ground truth is all NaN for certain reasons, do not compute loss since it results in NaN
+    if notnan == 0:
+        print("Found all NaN ground truth")
+        return kpts_pred.new_zeros(())
     return loss_fcn(kpts_gt, kpts_pred) / notnan
 
-def l1_loss(kpts_gt, kpts_pred):
-    return F.l1_loss(kpts_gt, kpts_pred, reduction="sum")
+##################################################################################################
+# SINGLE ANIMAL
+##################################################################################################
 
-def temporal_loss(kpts_pred):
+def l1_loss(kpts_gt, kpts_pred):
+    kpts_gt, kpts_pred, notnan = mask_nan(kpts_gt, kpts_pred)
+    # when ground truth is all NaN for certain reasons, do not compute loss since it results in NaN
+    if notnan == 0:
+        print("Found all NaN ground truth")
+        return kpts_pred.new_zeros(())
+    return F.l1_loss(kpts_gt, kpts_pred, reduction="sum") / notnan
+
+def temporal_loss(kpts_gt, kpts_pred):
     """
     Unsupervised temporal consistency loss.
-    kpts_pred: reshaped tensor [n_chunks, temporal_chunk_size, n_joints, 3]
+    kpts_pred: reshaped tensor [n_chunks, temporal_chunk_size, 3, n_joints]
     """
-    diff = torch.diff(kpts_pred, dim=1) # [temporal_chunk_size-1, n_joints, 3]
+    diff = torch.diff(kpts_pred, dim=1) # [n_chunks, temporal_chunk_size-1, n_joints, 3]
     loss_temp = torch.abs(diff).mean()
+
     return loss_temp
 
-def body_symmetry_loss(animal):
-    limbL = torch.as_tensor([limbs[0] for limbs in SYMMETRY[animal]])
-    limbR = torch.as_tensor([limbs[1] for limbs in SYMMETRY[animal]])
-    def _body_symmetry_loss(kpts_pred):
+# def body_symmetry_loss(animal="mouse22"):
+#     limbL = torch.as_tensor([limbs[0] for limbs in SYMMETRY[animal]])
+#     limbR = torch.as_tensor([limbs[1] for limbs in SYMMETRY[animal]])
+#     def _body_symmetry_loss(kpts_gt, kpts_pred):
+#         """
+#         Unsupervised body symmetry loss.
+#         kpts_pred: tensor [batch_size, n_joints, 3]
+#         """
+#         len_L = torch.diff(kpts_pred[:, limbL, :] ** 2, dim=2).mean(-1).mean(-1).sqrt()
+#         len_R = torch.diff(kpts_pred[:, limbR, :] ** 2, dim=2).mean(-1).mean(-1).sqrt()
+#         loss = (len_L - len_R).abs().mean()
+#         return loss
+#     return _body_symmetry_loss
+animal = "mouse22"
+limbL = torch.as_tensor([limbs[0] for limbs in SYMMETRY[animal]])
+limbR = torch.as_tensor([limbs[1] for limbs in SYMMETRY[animal]])
+
+def body_symmetry_loss(kpts_gt, kpts_pred):
+    """
+    Unsupervised body symmetry loss.
+    kpts_pred: tensor [batch_size, 3, n_joints]
+    """
+    len_L = (torch.diff(kpts_pred[:, :, limbL], dim=-1)**2).sum(1).sqrt().mean()
+    len_R = (torch.diff(kpts_pred[:, :, limbR], dim=-1)**2).sum(1).sqrt().mean()
+    loss = (len_L - len_R).abs()
+    return loss
+
+def silhouette_loss(kpts_gt, kpts_pred):
+    # y_true and y_pred will both have shape
+    # (n_batch, width, height, n_keypts)
+    reduce_axes = (1, 2, 3)
+    sil = torch.sum(kpts_pred * kpts_gt, dim=reduce_axes)
+    sil = -(sil+1e-12).log()
+    
+    return sil.mean()
+
+def separation_loss(delta=10):
+    def _separation_loss(kpts_gt, kpts_pred):
         """
-        Unsupervised body symmetry loss.
-        kpts_pred: tensor [batch_size, n_joints, 3]
+        Loss which penalizes 3D keypoint predictions being too close.
         """
-        len_L = torch.diff(kpts_pred[:, limbL, :] ** 2, dim=2).mean(-1).mean(-1).sqrt()
-        len_R = torch.diff(kpts_pred[:, limbR, :] ** 2, dim=2).mean(-1).mean(-1).sqrt()
-        loss = (len_L - len_R).abs().mean()
-        return loss
-    return _body_symmetry_loss
+        num_kpts = kpts_pred.shape[-1]
+
+        t1 = kpts_pred.repeat(1, 1, num_kpts)
+        t2 = kpts_pred.repeat(1, num_kpts, 1).reshape(t1.shape)
+
+        lensqr = ((t1 - t2)**2).sum(1)
+        sep = torch.maximum(delta - lensqr, 0.0).sum(1) / num_kpts**2
+
+        return sep.mean()
+    return _separation_loss
+
+##################################################################################################
+# SOCIAL ANIMALS
+##################################################################################################
+
+def pair_repulsion_loss(y_pred_s1, y_pred_s2):
+    """Unsupervised pairwise loss with respect to two subjects. 
+    The predictions should be as far as possible, i.e. repelling each other.
+    Input:
+        y_pred_s1, y_pred_s2: (B, N, 3)"""
+
+    return 1 / ((y_pred_s1 - y_pred_s2)**2).sum()
